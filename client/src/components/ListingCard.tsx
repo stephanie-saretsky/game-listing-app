46797af0bd9119210cbc046ff77d1748019d5b3a
Stephanie Saretsky <stephanie.saretsky@transit.app>
2021-11-02 09:13:09 -0400

    only show image if url exists

--- client/src/components/ListingCard.tsx
+++ client/src/components/ListingCard.tsx
@@ -16,11 +16,25 @@ const ListingCard = ({ listing }: ListingCardProps) => {
         }
     });
 
+    if (image[0].length === 0) {
+        return (
+            <div className="border border-snowgrey bg-gray-100 h-84 p-4 rounded-lg w-full">
+                <div className="flex flex-col items-center">
+                    <div className="text-xl font-bold mb-4">{listing.title}</div>
+                    <div className="font-bold mb-4">{listing.subtitle}</div>
+                    <div className="mb-4">{listing.description}</div>
+                    <div className="flex justify-center space-between text-xs">
+                        {tags?.join('  ')}
+                    </div>
+                </div>
+            </div>
+    );
+    }
     return (
             <div className="border border-snowgrey bg-gray-100 h-84 p-4 rounded-lg w-full">
                 <div className="flex flex-col items-center">
                     <div className="border border-black-200">
-                        <img src={`${image}`} width={200} alt="logo"/>
+                        <img src={`${image[0]}`} width={200} alt="logo"/>
                     </div>
                     <div className="text-xl font-bold mb-4">{listing.title}</div>
                     <div className="font-bold mb-4">{listing.subtitle}</div>
