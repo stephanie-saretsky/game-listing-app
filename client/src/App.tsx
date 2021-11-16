866b70fbea9b661ace3d8fdc7f5006a9754804f7
Stephanie Saretsky <stephanie.saretsky@transit.app>
2021-11-16 15:53:24 -0500

    lint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
 
   useEffect(() => {
     getListings();
-  });
+  }, [modalIsOpen]);
 
   if (!gameListings) {
     return <div />;
--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://sick:swum-fireball-dbuser!@sickcluster.w88al.mongodb.net/games?retryWrites=true&w=majority";
+const uri = "mongo url with password would go here";
 
 let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
